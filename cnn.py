526b222a399f8d72347e5df6888c7cd694f85831
Patryk Uchman <patrykuchmandev@gmail.com>
2019-04-20 18:39:33 +0200

    Added test images flag

--- cnn.py
+++ cnn.py
@@ -39,7 +39,7 @@ def load_dataset(image_dataset_path):
     return train_loader
 
 
-def train(model, device, train_loader, optimizer, epoch, log_interval=10):
+def train(model, device, train_loader, optimizer, epoch, log_interval=100):
     model.train()
     correct_predictions, all_predictions = 0, 0
     for batch_idx, (data, target) in enumerate(train_loader):
@@ -63,6 +63,23 @@ def train(model, device, train_loader, optimizer, epoch, log_interval=10):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
 
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
 
 if __name__=='__main__':
@@ -70,13 +87,15 @@ if __name__=='__main__':
     parser.add_argument('--use_cuda', action='store_true', default=False,
                         help='enables CUDA training')
     parser.add_argument('--train_images', help='path to train images folder')
-    #parser.add_argument('--test_images', help='path to test images folder')
+    parser.add_argument('--test_images', help='path to test images folder')
     args = parser.parse_args()
 
     use_cuda = args.use_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     train_loader = load_dataset(args.train_images)
+    test_loader = load_dataset(args.test_images)
     model = Net().to(device)
     optimizer = optim.Adam(model.parameters())
     for epoch in range(1, 100):
         train(model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
