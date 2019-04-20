6d0478a35e189ffe20b7b439188ce72f8d65f42c
Patryk Uchman <patrykuchmandev@gmail.com>
2019-04-20 20:12:04 +0200

    Added pytorch batch norm

--- cnn.py
+++ cnn.py
@@ -9,16 +9,18 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=6, kernel_size=3, padding=(1,1))
+        self.batch_norm1 = nn.BatchNorm2d(num_features=3)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, padding=(1,1))
+        self.batch_norm2 = nn.BatchNorm2d(num_features=6)
 
         self.fc1 = nn.Linear(in_features=16 * 25 * 25, out_features=120)
         self.fc2 = nn.Linear(in_features=120, out_features=84)
         self.fc3 = nn.Linear(in_features=84, out_features=96)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv1(self.batch_norm1(x))))
+        x = self.pool(F.relu(self.conv2(self.batch_norm2(x))))
         x = x.view(-1, 16 * 25 * 25)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
